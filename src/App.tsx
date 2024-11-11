44e952db8e385cdb854abb5e6f7332a6422c2a95
rumdien <rumdien@gmail.com>
2024-11-12 02:43:40 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl text-red-100 font-bold">Hello, Vite + React + TailwindCSS!</h1>
+    </div>
+  );
+}
